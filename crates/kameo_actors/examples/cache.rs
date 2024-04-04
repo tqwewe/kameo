f0c8d15e9436e802d30acfa81e9b05bc8926dc6e
Ari Seyhun <ariseyhun@live.com.au>
2024-04-04 14:41:23 +0800

    feat: add cache reply
    feat: add cache reply
    
    feat: add cache reply

---
+++ crates/kameo_actors/examples/cache.rs
@@ -0,0 +1,136 @@
+use std::{
+    collections::HashMap,
+    convert::Infallible,
+    time::{Duration, Instant},
+};
+
+use futures::FutureExt;
+use kameo::{actor, actor_cache::CacheReply, Actor};
+use tracing_subscriber::EnvFilter;
+
+#[derive(Actor, Default)]
+struct DbCache {
+    cache: HashMap<String, String>,
+}
+
+#[actor]
+impl DbCache {
+    #[message]
+    async fn get_key(&self, key: String) -> Result<CacheReply<String>, Infallible> {
+        let cache_reply = match self.cache.get(&key).cloned() {
+            Some(value) => CacheReply::hit(value),
+            None => {
+                CacheReply::miss(
+                    self.actor_ref(),
+                    key,
+                    async move {
+                        tokio::time::sleep(Duration::from_secs(2)).await;
+                        "howdy".to_string()
+                    },
+                    |state, key, value| {
+                        async move {
+                            state.cache.insert(key.to_string(), value.to_string());
+                        }
+                        .boxed()
+                    },
+                )
+                .await
+            }
+        };
+
+        Ok(cache_reply)
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_env_filter("warn".parse::<EnvFilter>().unwrap())
+        .without_time()
+        .with_target(false)
+        .init();
+
+    let db_cache = kameo::spawn(DbCache::default());
+
+    let start = Instant::now();
+    let (value_1, value_2) = tokio::try_join!(
+        tokio::spawn({
+            let db_cache = db_cache.clone();
+            async move {
+                db_cache
+                    .send(GetKey {
+                        key: "hi".to_string(),
+                    })
+                    .await
+                    .unwrap()
+                    .await
+            }
+        }),
+        tokio::spawn({
+            let db_cache = db_cache.clone();
+            async move {
+                db_cache
+                    .send(GetKey {
+                        key: "hi".to_string(),
+                    })
+                    .await
+                    .unwrap()
+                    .await
+            }
+        }),
+    )?;
+    let dur = start.elapsed();
+    dbg!(value_1?, value_2?, dur.as_millis());
+
+    let start = Instant::now();
+    let value = db_cache
+        .send(GetKey {
+            key: "hi".to_string(),
+        })
+        .await?
+        .await?;
+    let dur = start.elapsed();
+    dbg!(value, dur.as_millis());
+
+    let start = Instant::now();
+    let value = db_cache
+        .send(GetKey {
+            key: "hi".to_string(),
+        })
+        .await?
+        .await?;
+    let dur = start.elapsed();
+    dbg!(value, dur.as_millis());
+
+    let start = Instant::now();
+    let value = db_cache
+        .send(GetKey {
+            key: "there".to_string(),
+        })
+        .await?
+        .await?;
+    let dur = start.elapsed();
+    dbg!(value, dur.as_millis());
+
+    let start = Instant::now();
+    let value = db_cache
+        .send(GetKey {
+            key: "there".to_string(),
+        })
+        .await?
+        .await?;
+    let dur = start.elapsed();
+    dbg!(value, dur.as_millis());
+
+    let start = Instant::now();
+    let value = db_cache
+        .send(GetKey {
+            key: "there".to_string(),
+        })
+        .await?
+        .await?;
+    let dur = start.elapsed();
+    dbg!(value, dur.as_millis());
+
+    Ok(())
+}
---
+++ crates/kameo_actors/src/cache.rs
@@ -0,0 +1,226 @@
+use std::{
+    any,
+    collections::HashMap,
+    convert::Infallible,
+    future::Future,
+    marker::PhantomData,
+    pin::Pin,
+    task::{ready, Context, Poll},
+};
+
+use futures::future::BoxFuture;
+use lazy_static::lazy_static;
+use tokio::sync::watch::{self, error::RecvError, Receiver, Sender};
+use tokio_util::sync::ReusableBoxFuture;
+
+use kameo::{Actor, ActorRef, Message};
+
+type AnyReceiverValue = Option<Box<dyn any::Any + Send + Sync>>;
+
+lazy_static! {
+    static ref CACHE_REGISTRY_ACTOR: ActorRef<CacheRegistry> =
+        kameo::spawn(CacheRegistry::default());
+}
+
+#[derive(Default)]
+struct CacheRegistry {
+    futures: HashMap<(&'static str, String), Receiver<AnyReceiverValue>>,
+}
+
+impl Actor for CacheRegistry {
+    fn name() -> &'static str {
+        "CacheRegistry"
+    }
+}
+
+struct AddCacheReply<T, A, F, F2> {
+    actor_ref: ActorRef<A>,
+    key: String,
+    future: F,
+    update_cache: F2,
+    phantom: PhantomData<T>,
+}
+
+impl<T, A, F, F2> Message<AddCacheReply<T, A, F, F2>> for CacheRegistry
+where
+    T: Send + Sync + 'static,
+    A: Actor + Send + 'static,
+    F: Future<Output = T> + Send + 'static,
+    F2: for<'a> Fn(&'a mut A, &'a str, &'a T) -> BoxFuture<'a, ()> + Send + 'static,
+{
+    type Reply = Result<Receiver<AnyReceiverValue>, Infallible>;
+
+    async fn handle(&mut self, msg: AddCacheReply<T, A, F, F2>) -> Self::Reply {
+        let key = (A::name(), msg.key);
+        let rx = match self.futures.get(&key).cloned() {
+            Some(rx) => rx,
+            None => {
+                println!("insert pending cache reply for key {}", &key.1);
+                let (tx, rx) = watch::channel(None);
+                self.futures.insert(key.clone(), rx.clone());
+                tokio::spawn({
+                    let key = key.clone();
+                    async move {
+                        println!("starting future");
+                        let value = msg.future.await;
+                        println!("future finished");
+                        let res = msg
+                            .actor_ref
+                            .send(CacheReplyFinished {
+                                key: key.1.clone(),
+                                value,
+                                tx,
+                                f: Box::new(msg.update_cache),
+                            })
+                            .await;
+                        if res.is_err() {
+                            let _ = CACHE_REGISTRY_ACTOR.send_async(RemoveCacheReply {
+                                actor: A::name(),
+                                key: key.1,
+                            });
+                        }
+                    }
+                });
+
+                rx
+            }
+        };
+
+        Ok(rx)
+    }
+}
+
+struct RemoveCacheReply {
+    actor: &'static str,
+    key: String,
+}
+
+impl Message<RemoveCacheReply> for CacheRegistry {
+    type Reply = Result<Option<Receiver<AnyReceiverValue>>, Infallible>;
+
+    async fn handle(&mut self, msg: RemoveCacheReply) -> Self::Reply {
+        println!("remove pending cache reply for key {}", &msg.key);
+        Ok(self.futures.remove(&(msg.actor, msg.key)))
+    }
+}
+
+struct CacheReplyFinished<A, T> {
+    key: String,
+    value: T,
+    tx: Sender<AnyReceiverValue>,
+    f: Box<dyn for<'a> Fn(&'a mut A, &'a str, &'a T) -> BoxFuture<'a, ()> + Send>,
+}
+
+trait CacheActor {}
+
+impl<A, T> Message<CacheReplyFinished<A, T>> for A
+where
+    A: Actor + Send + 'static,
+    T: Send + Sync + 'static,
+{
+    type Reply = Result<(), ()>;
+
+    async fn handle(&mut self, msg: CacheReplyFinished<A, T>) -> Self::Reply {
+        (msg.f)(self, &msg.key, &msg.value).await;
+        let _ = CACHE_REGISTRY_ACTOR
+            .send(RemoveCacheReply {
+                actor: A::name(),
+                key: msg.key,
+            })
+            .await;
+        let _ = msg
+            .tx
+            .send(Some(Box::new(msg.value) as Box<dyn any::Any + Send + Sync>));
+
+        Ok(())
+    }
+}
+
+pub struct CacheReply<T> {
+    inner: CacheReplyInner<T>,
+}
+
+enum CacheReplyInner<T> {
+    Hit(T),
+    Miss {
+        rx_fut: ReusableBoxFuture<'static, (Result<(), RecvError>, Receiver<AnyReceiverValue>)>,
+    },
+}
+
+impl<T> CacheReply<T> {
+    pub fn hit(value: T) -> CacheReply<T> {
+        CacheReply {
+            inner: CacheReplyInner::Hit(value),
+        }
+    }
+
+    pub async fn miss<A, F, F2>(
+        actor_ref: ActorRef<A>,
+        key: String,
+        future: F,
+        update_cache: F2,
+    ) -> CacheReply<T>
+    where
+        T: Clone + Send + Sync + 'static,
+        A: Actor + Send + 'static,
+        F: Future<Output = T> + Send + 'static,
+        F2: for<'a> Fn(&'a mut A, &'a str, &'a T) -> BoxFuture<'a, ()> + Send + 'static,
+    {
+        let rx = CACHE_REGISTRY_ACTOR
+            .send(AddCacheReply {
+                actor_ref,
+                key,
+                future,
+                update_cache,
+                phantom: PhantomData,
+            })
+            .await
+            .unwrap();
+
+        CacheReply {
+            inner: CacheReplyInner::Miss {
+                rx_fut: ReusableBoxFuture::new(make_future(rx)),
+            },
+        }
+    }
+}
+
+async fn make_future(
+    mut rx: Receiver<AnyReceiverValue>,
+) -> (Result<(), RecvError>, Receiver<AnyReceiverValue>) {
+    let result = rx.changed().await;
+    (result, rx)
+}
+
+impl<T> Future for CacheReply<T>
+where
+    T: Clone + Unpin + 'static,
+{
+    type Output = Result<T, RecvError>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match &mut self.inner {
+            CacheReplyInner::Hit(value) => Poll::Ready(Ok(value.clone())),
+            CacheReplyInner::Miss { rx_fut } => loop {
+                let (result, mut rx) = ready!(rx_fut.poll(cx));
+                match result {
+                    Ok(_) => {
+                        let received = rx
+                            .borrow_and_update()
+                            .as_ref()
+                            .map(|value| value.downcast_ref::<T>().unwrap().clone());
+                        rx_fut.set(make_future(rx));
+                        match received {
+                            Some(value) => return Poll::Ready(Ok(value)),
+                            None => continue,
+                        }
+                    }
+                    Err(err) => {
+                        rx_fut.set(make_future(rx));
+                        return Poll::Ready(Err(err));
+                    }
+                }
+            },
+        }
+    }
+}
