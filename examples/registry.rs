b5da4bdbd8927cc7a3021cc8e4e94bf6cbe8f68b
Ari Seyhun <ariseyhun@live.com.au>
2025-01-13 00:06:56 +0800

    fix: failing registry tests

--- examples/registry.rs
+++ examples/registry.rs
@@ -1,14 +1,33 @@
 use kameo::{actor::ActorRef, Actor};
 
 #[derive(Actor)]
+#[cfg_attr(feature = "remote", derive(kameo::RemoteActor))]
 pub struct MyActor;
 
+#[cfg(not(feature = "remote"))]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let actor_ref = kameo::spawn(MyActor);
     actor_ref.register("my awesome actor")?;
 
     let other_actor_ref = ActorRef::<MyActor>::lookup("my awesome actor")?.unwrap();
+
+    assert_eq!(actor_ref.id(), other_actor_ref.id());
+
+    Ok(())
+}
+
+#[cfg(feature = "remote")]
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _swarm = kameo::remote::ActorSwarm::bootstrap()?;
+
+    let actor_ref = kameo::spawn(MyActor);
+    actor_ref.register("my awesome actor").await?;
+
+    let other_actor_ref = ActorRef::<MyActor>::lookup("my awesome actor")
+        .await?
+        .unwrap();
 
     assert_eq!(actor_ref.id(), other_actor_ref.id());
 
