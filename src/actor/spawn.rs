3d49b26d4341ed2284f3cd26c7535384d7304817
Ari Seyhun <ariseyhun@live.com.au>
2025-11-08 03:12:53 +1100

    chore: fix clippy lint

--- src/actor/spawn.rs
+++ src/actor/spawn.rs
@@ -265,10 +265,8 @@ async fn abortable_actor_loop<A>(
 where
     A: Actor,
 {
-    if startup_finished {
-        if let ControlFlow::Break(reason) = state.handle_startup_finished().await {
-            return reason;
-        }
+    if startup_finished && let ControlFlow::Break(reason) = state.handle_startup_finished().await {
+        return reason;
     }
     loop {
         let reason = recv_mailbox_loop(state, &mut mailbox_rx, startup_result).await;
